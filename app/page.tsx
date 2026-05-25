9c7717f4b52e448659d18144c489cf8b238b4065
Mohamed Elnafad <m.elnafad@edfapay.com>
2026-05-25 22:51:44 +0300

    fix: update middleware

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
   if (session) {
     redirect("/dashboard");
   } else {
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,4 @@
-import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-import type { NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*"],
